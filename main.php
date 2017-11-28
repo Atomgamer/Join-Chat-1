1012536f72eef008efa642eb9e387513fac94ef0
Atomgamer <33846625+Atomgamer@users.noreply.github.com>
2017-11-28 16:47:53 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
 <?php
-namespace JoinChat1;
+namespace JoinChat7;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\player\PlayerJoinEvent;
+class Main extends PluginBase implements Listener {
+
+        public function onEnable 
+                $this->getServer()->getLogger()->info("JoinMessage enabled!");
+                $this->getServer()->getManager()->registerEvents($this, $this);
+
+                
+        )
